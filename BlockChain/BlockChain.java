cf9e634bb12aa9b6d2a731ba3aacf6f260e90ca4
Allen Kan <akanch95@gmail.com>
2020-01-20 22:30:52 -0800

    Store blocks in an arraylist

--- BlockChain/BlockChain.java
+++ BlockChain/BlockChain.java
@@ -1,13 +1,16 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+
 public class BlockChain {
 	
-	public static void main(String[] args) {
-		Block genesisBlock = new Block("first block", "0");
-		System.out.println("Hash for block 1: " + genesisBlock.hash);
+	public static ArrayList<Block> blockchain = new ArrayList<Block>();
+	
+	public static void main(String[] args) {	
+		//add our blocks to the blockchain ArrayList:
+		blockchain.add(new Block("first block", "0"));		
+		blockchain.add(new Block("second block",blockchain.get(blockchain.size()-1).hash)); 
+		blockchain.add(new Block("third block",blockchain.get(blockchain.size()-1).hash));
 		
-		Block secondBlock = new Block("second block", "0");
-		System.out.println("Hash for block 2: " + secondBlock.hash);
-		
-		Block thirdBlock = new Block("third block", "0");
-		System.out.println("Hash for block 3: " + thirdBlock.hash);
+		System.out.println(Arrays.toString(blockchain.toArray()));
 	}
 }
